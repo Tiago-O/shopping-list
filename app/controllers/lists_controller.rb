a758635576d986e0763b06c5a46a91189b822736
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-09-12 19:19:44 +0100

    implemented add list form and controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,10 +2,23 @@ class ListsController < ApplicationController
   before_action :set_list, only: %i[show]
 
   def index
-    @lists = List.all
+    @lists = List.where("user_id = ?", current_user)
+    @new_list = List.new
   end
 
   def show
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      flash[:alert] = 'you added a list!'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = 'add a list failed'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :lists, only: [:index, :show]
+  # get 'lists', to: 'lists#index'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  resources :lists, only: [:index, :show, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
