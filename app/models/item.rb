fb81396c553a479eb6c4b975ed7c4a84f4cc1375
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-09-12 11:24:04 +0100

    create model item, updated models user and list, created validations and associations

---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+  belongs_to :list
+
+  validates :name, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 1 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :lists, dependent: :destroy
+  has_many :items, through: :lists, dependent: :destroy
+
+  validates :name, presence: true
 end
