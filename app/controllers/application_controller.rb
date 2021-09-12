7d307fba8b9beb5e3d0fbc659b42436312cd5a7c
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-09-12 18:17:21 +0100

    implemented routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  # def after_sign_in_path_for(resource)
-  #   lists_path
-  # end
+  def after_sign_in_path_for(resource)
+    lists_path
+  end
 end
